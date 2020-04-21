0793af8f1b11b4612a2b0e7d095398f213e319b2
Roundy <nroundy801@gmail.com>
2020-04-21 09:58:08 -0600

    added robohash to car.component.jsx file

--- src/Components/Card/card.component.jsx
+++ src/Components/Card/card.component.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import "./card.style.css"
 
 export const Card = (props) => (
-  <div className="">
+  <div className="card-container">
+    <img src={`https://robohash.org/${props.brewery.id}?set=set4&size=180x180`} alt=""/>
     <h4>{props.brewery.name}</h4>
   </div>
 );
