f5250ca17c4ed2800687bab8ba6dbd2cbc97e4bd
Roundy <nroundy801@gmail.com>
2020-04-21 10:03:11 -0600

    pulling state information from the api off of props.

--- src/Components/Card/card.component.jsx
+++ src/Components/Card/card.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import "./card.style.css"
+import "./card.style.css";
 
 export const Card = (props) => (
   <div className="card-container">
-    <img src={`https://robohash.org/${props.brewery.id}?set=set4&size=180x180`} alt=""/>
-    <h4>{props.brewery.name}</h4>
+    <img
+      src={`https://robohash.org/${props.brewery.id}?set=set4&size=180x180`}
+      alt=""
+    />
+    <h4><strong>Brewery: </strong>{props.brewery.name}</h4>
+    <p><strong>State: </strong>{props.brewery.state}</p>
   </div>
 );
