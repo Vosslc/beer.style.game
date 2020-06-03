f19dc019bfbdef472a6cf837cad51ccc958bc882
Roundy <nroundy801@gmail.com>
2020-06-03 11:47:32 -0600

    changed some card.styling. The front of the filp card is now off blue and that back of the card now has a random beer image

--- src/Components/Card/card.component.jsx
+++ src/Components/Card/card.component.jsx
@@ -3,10 +3,14 @@ import "./card.style.css";
 
 const pTagFontColor = {
   color: "black",
-  fontSize: "15px",
-  display: "flex"
+  fontSize: "20px",
+  display: "flex",
+  background: "#e7d3ce",
+  background: "-webkit-linear-gradient(45deg, #e7d3ce 0%, #596a72 100%)",
+  background: "-o-linear-gradient(45deg, #cedce7 0%, #596a72 100%)",
+  background: "linear-gradient(45deg, #cedce7 0%, #596a72 100%)",
+  opacity: ".9",
 };
-
 
 export const Card = (props) => (
   // <div className="card-container">
@@ -30,12 +34,11 @@ export const Card = (props) => (
       <div class="container">
         <div
           className="front"
-          style={{
-            // *using unsplash brewery images for card background
-            backgroundImage: `url("https://source.unsplash.com/750x450/?brewery")`,
-            // backgroundImage: `url("https://placeimg.com/720/540/nature")`,
-          }}
-          
+          // style={{
+          //   // *using unsplash brewery images for card background
+          //   backgroundImage: `url("https://source.unsplash.com/750x450/?brewery")`,
+          //   // backgroundImage: `url("https://placeimg.com/720/540/nature")`,
+          // }}
         >
           <div className="inner">
             <img className="beer-img" src={props.brewery.image_url} alt="" />
@@ -43,10 +46,25 @@ export const Card = (props) => (
             <p>{props.brewery.tagline}</p>
           </div>
         </div>
-        <div className="back">
+        <div
+          className="back"
+          style={{
+            // *using unsplash brewery images for card background
+            backgroundImage: `url("https://source.unsplash.com/750x450/?brewery")`,
+            // backgroundImage: `url("https://placeimg.com/720/540/nature")`,
+          }}
+        >
           <div className="inner">
-            <p>Beer Description: </p>
-              <hr/>
+            <p
+              style={{
+                color: "black",
+                fontSize: "40px",
+                fontFamily: "Titillium Web"
+              }}
+            >
+              <strong>Beer Description:</strong>
+            </p>
+            <hr />
             <p className="beer-card-description" style={pTagFontColor}>
               {" "}
               <p>
