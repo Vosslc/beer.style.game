23b29a3072a1a90bb99e13fc0ea8287c6264cddb
Roundy <nroundy801@gmail.com>
2020-06-08 20:18:33 -0600

    cleaned up some commented out code

--- src/Components/Card/card.component.jsx
+++ src/Components/Card/card.component.jsx
@@ -3,32 +3,14 @@ import "./card.style.css";
 
 const pTagFontColor = {
   color: "black",
-  fontSize: "20px",
+  fontSize: "17px",
   display: "flex",
-  // background: "#e7d3ce",
   background: "-webkit-linear-gradient(45deg, #e7d3ce 0%, #596a72 100%)",
-  // background: "-o-linear-gradient(45deg, #cedce7 0%, #596a72 100%)",
-  // background: "linear-gradient(45deg, #cedce7 0%, #596a72 100%)",
   opacity: ".9",
+  overflow: 'hidden',
 };
 
 export const Card = (props) => (
-  // <div className="card-container">
-  //   <img
-  //     // src={`https://robohash.org/${props.brewery.id}?set=set4&size=180x180`}
-  //     // src={`https://images.punkapi.com/v2/${props.brewery.id.image_url}`}
-  //     // src={`https://images.punkapi.com/${props.brewery.id.image_url}`}
-  //     alt=""
-  //   />
-  //   <img className="beer-img" src={props.brewery.image_url} alt=""/>
-  //   <p><strong>Beer: </strong>{props.brewery.name}</p>
-  //   <p><strong>Abv: </strong>{props.brewery.abv}%</p>
-  //   <p><strong>Tagline: </strong>{props.brewery.tagline}</p>
-  //   <p className="beer-card-description"><strong>Description: </strong>{props.brewery.description}</p>
-  // </div>
-
-  // <div className="wrapper, card-container">
-
   <div className="cols">
     {/* ? ADD THIS TO THE DIV BELOW onTouchStart={this.classList.toggle('hover')} */}
     <div className="col">
@@ -56,18 +38,16 @@ export const Card = (props) => (
           }}
         >
           <div className="inner">
-            <p
-              style={{
-                color: "black",
-                fontSize: "40px",
-                fontFamily: "Titillium Web",
-              }}
-            >
-              <strong>Beer Description:</strong>
-            </p>
-
             <div className="beer-card-description" style={pTagFontColor}>
-              {" "}
+              <p
+                style={{
+                  color: "black",
+                  fontSize: "20px",
+                  fontFamily: "Titillium Web",
+                }}
+              >
+                Beer Description
+              </p>{" "}
               <p>
                 Abv : {""}
                 {props.brewery.abv}%
